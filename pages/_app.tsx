5e7d69ea89015fd322cc9bfaa35301814b492e34
mutiaradevita <mutiaradevitaekaputri>
2024-03-20 14:18:07 +0700

    praktikum 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"; // import css bootstrap
 import "@/styles/globals.css";
+import { useEffect } from "react"; // import hook untuk javascript bootstrap
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
